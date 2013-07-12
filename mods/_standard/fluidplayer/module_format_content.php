ec7e52f372fb452a1b641c15d60516ce1ac75ceb
mancoolgunda <abhinav.koppula@gmail.com>
2013-07-12 07:47:14 +0530

    Graceful degradation to Flowplayer from FluidPlayer for flv files

--- mods/_standard/fluidplayer/module_format_content.php
+++ mods/_standard/fluidplayer/module_format_content.php
@@ -5,6 +5,31 @@ $AT_BASE_HREF = AT_print(AT_BASE_HREF,'url.base');
 $media_replace = array();
 $media_matches = array();
 $fluidplayerholder_class = "player";
+$caption_init_flowplayer = "
+    captions: {
+            url: '".AT_BASE_HREF."mods/_standard/flowplayer/flowplayer.captions-3.2.9.swf',
+            // pointer to a content plugin
+            captionTarget: 'content'
+        },
+        content: {
+            url: '".AT_BASE_HREF."mods/_standard/flowplayer/flowplayer.content-3.2.8.swf',
+            bottom: 25,
+            width: '80%',
+            height:40,
+            backgroundColor: '#000000',
+            backgroundGradient: 'low',
+            borderRadius: 4,
+            border: 0,
+            style: {
+                'body': {
+                fontSize: '14',
+                fontFamily: 'Arial',
+                textAlign: 'center',
+                color: '#FFFFFF'
+                }
+            }
+        }
+";
 $caption_init = ",
     captions: [
     {
@@ -64,6 +89,20 @@ $media_replace[] = "<div>\n".
                             }); 
                         });
                         </script>\n".
+                        "<h2 class='box' style='width:200px;'>
+                              <span>Shortcuts</span>
+                              <a href='#' class='flowplayer_shortcuts_link' title='Show Keyboard Shortcuts' onclick = 'show_flowplayer_shortcuts(this);return false;'></a>
+                              <div class='box' style='display:none;'>
+                                <ul class='social_side_menu'>
+                                    <li>Play/Pause: Spacebar Key</li>
+                                    <li>Forward/Backward: Shift + Left/Right Key</li>
+                                    <li>Volume Up: Shift + Up Key</li>
+                                    <li>Volume Down: Shift + Down Key</li>
+                                    <li>Transcripts Toggle: Shift + t key</li>
+                                    <li>Fullscreen Toggle: Shift + f key</li>
+                                </ul>
+                              </div>
+                        </h2>\n".
                    "</div>\n";
 
 // .webm
@@ -105,8 +144,83 @@ $media_replace[] = "<div>\n".
                             }); 
                         });
                         </script>\n".
+                        "<h2 class='box' style='width:200px;'>
+                              <span>Shortcuts</span>
+                              <a href='#' class='flowplayer_shortcuts_link' title='Show Keyboard Shortcuts' onclick = 'show_flowplayer_shortcuts(this);return false;'></a>
+                              <div class='box' style='display:none;'>
+                                <ul class='social_side_menu'>
+                                    <li>Play/Pause: Spacebar Key</li>
+                                    <li>Forward/Backward: Shift + Left/Right Key</li>
+                                    <li>Volume Up: Shift + Up Key</li>
+                                    <li>Volume Down: Shift + Down Key</li>
+                                    <li>Transcripts Toggle: Shift + t key</li>
+                                    <li>Fullscreen Toggle: Shift + f key</li>
+                                </ul>
+                              </div>
+                        </h2>\n".
                    "</div>\n";
 
+// .flv
+preg_match_all("#\[media[0-9a-z\|]*([\s]?captions=[.\w\d]+[^\s\"]+\.srt)*([\s]?transcripts=[.\w\d]+[^\s\"]+\.json)*\]([.\w\d]+[^\s\"]+)\.flv\[/media\]#i",$_input,$media_matches[],PREG_SET_ORDER);
+
+if (isset($_SESSION['flash']) && $_SESSION['flash'] == "yes") {
+	$media_replace[] = "<div>\n".
+	                   "  <div class=\"##DIVCLASS##\" style=\"display:block;width:##WIDTH##px;height:##HEIGHT##px;\" ></div>\n".
+                           "  <script type=\"text/javascript\">
+                               \$f(\".##DIVCLASS##\", {
+                                src: '".AT_BASE_HREF."mods/_standard/flowplayer/flowplayer-3.2.16.swf',
+                                SeamlessTabbing: false
+                                }, {
+                                onKeyPress: function(code) {
+                                    switch(code) {
+                                        case 70: this.toggleFullscreen();           //F key
+                                        break;
+                                        case 36: this.stop();this.play();           //Home key
+                                        break;
+                                        case 67: this.getPlugin('content').toggle();//C key
+                                        break;
+                                    }
+                                },
+                                plugins: {
+                                controls: {
+                                autoHide: false,
+          
+                                tooltips: {
+                                    buttons: true
+                                    }
+                                },
+                                ##CAPTION_CODE##
+                                },
+                                clip: {
+                                url: \"##MEDIA1##.flv\",
+                                scaling: 'fit',
+                                autoPlay: false,
+                                baseUrl: \"".AT_BASE_HREF."get.php/".$_content_base_href."\",
+                                autoBuffering: true,
+                                ##CAPTIONS##
+                                }
+                                });
+                              </script>\n".
+                              "<h2 class='box' style='width:200px;'>
+                              <span>Shortcuts</span>
+                              <a href='#' class='flowplayer_shortcuts_link' title='Show Keyboard Shortcuts' onclick = 'show_flowplayer_shortcuts(this);return false;'></a>
+                              <div class='box' style='display:none;'>
+                                <ul class='social_side_menu'>
+                                    <li>Play/Pause: Spacebar Key</li>
+                                    <li>Restart: Home Key</li>
+                                    <li>Volume Up: Up Key</li>
+                                    <li>Volume Down: Down Key</li>
+                                    <li>Captions Toggle: C key</li>
+                                    <li>Fullscreen: F key</li>
+                                </ul>
+                              </div>
+                              </h2>\n".
+	                   "</div>\n";
+} else {
+	$media_replace[] = "<div>\n".
+	                   "  <a href=\"##MEDIA1##.flv\">##MEDIA1##.flv</a>\n".
+	                   "</div>\n";
+}
 
 $holder_counter = 0;
 for ($i=0;$i<count($media_replace);$i++){
@@ -133,6 +247,11 @@ for ($i=0;$i<count($media_replace);$i++){
                     $media_input = str_replace("##CAPTION_CODE##", $caption_init, $media_input);
                     $media_input = str_replace("##CAPTIONS##", $matches[1], $media_input);
                 }
+                else if((!empty($media[1])) && (preg_match("/captions=([.\w\d]+[^\s\"]+\.srt)/", $media[1], $matches)))
+                {
+                    $media_input = str_replace("##CAPTION_CODE##", $caption_init_flowplayer, $media_input);
+                    $media_input = str_replace("##CAPTIONS##", 'captionUrl:"'.$matches[1].'"', $media_input);
+                }
                 else
                 {
                     $media_input = str_replace("##CAPTION_CODE##", '', $media_input);
@@ -158,4 +277,37 @@ for ($i=0;$i<count($media_replace);$i++){
 		$_input = str_replace($media[0],$media_input,$_input);
 	}
 }
+global $rtl;
+
+$tree_collapse_icon = AT_BASE_HREF.find_image($rtl.'tree/tree_collapse.gif', '');
+$tree_expand_icon = AT_BASE_HREF.find_image($rtl.'tree/tree_expand.gif', '');
+
+$_input.='
+    <script type="text/javascript">
+    ATutor.course.collapse_icon = "'. AT_print($tree_collapse_icon, 'url.tree').'";
+    ATutor.course.expand_icon = "'.AT_print($tree_expand_icon,  'url.tree').'";
+        
+    $(".flowplayer_shortcuts_link").html("<img src="+ATutor.course.expand_icon+" alt=\'Show Keyboard Shortcuts\' title=\'Show Keyboard Shortcuts\' class=\'shortcut_icon\' >");
+    function show_flowplayer_shortcuts(id) {
+    if($(id).find("img").attr("src") == ATutor.course.expand_icon) {
+        $(id).find("img").attr({
+            "src": ATutor.course.collapse_icon,
+            "alt": "Hide Keyboard Shortcuts",
+            "title": "Hide Keyboard Shortcuts"
+            })
+        $(id).attr("title", "Hide Keyboard Shortcuts");
+    }
+    else {
+        $(id).find("img").attr({
+            "src": ATutor.course.expand_icon,
+            "alt": "Show Keyboard Shortcuts",
+            "title": "Show Keyboard Shortcuts"
+            })
+        $(id).attr("title", "Show Keyboard Shortcuts");
+    }
+    ch = $(id).siblings(".box");
+    ch.slideToggle();
+    };
+    </script>
+';
 ?>
