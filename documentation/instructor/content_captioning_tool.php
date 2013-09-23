8bb7aa40eaf1eb872d0ebc0c14bce4c55c9d8c21
mancoolgunda <abhinav.koppula@gmail.com>
2013-09-24 00:05:26 +0530

    Captioning Tool Handbook documentation improved

--- documentation/instructor/content_captioning_tool.php
+++ documentation/instructor/content_captioning_tool.php
@@ -1,10 +1,41 @@
-<?php require('../common/body_header.inc.php'); $lm = '$LastChangedDate: 2013-09-18 10:03:20 -0500 (Wed, 18 Sept 2013) $'; ?>
+<?php require('../common/body_header.inc.php');
+$lm = '$LastChangedDate: 2013-09-18 10:03:20 -0500 (Wed, 18 Sept 2013) $'; ?>
 
 <h2>Captioning Tool</h2>
 <p>If <em>media tags</em> are specified in the Content tab, an instructor can make use of this Captioning Tool to create subtitles for the specified videos.</p> 
-<p>One can click the "Subtitle Me" link present under each video.</p>
-<p>A pop-up would be shown which would allow one to select options for the captioning process. </p>
-<p>After following the instructions, the instructor would be able to generate the subtitle file for any video.</p>
+<b>Step 1:</b>
+<p>One can see the list of videos which have been specified in the content.</p>
+<p>On clicking the "Subtitle Me" link present under each video, a pop-up appears.</p>
+<p>If the video is already being moderated(subtitled) by some team, then such a pop-up appears. If not, you would be shown a pop-up similar to Step 2.</p>
+<p><img src="../../images/captioning1.png" /><br/>
+    You can now click on the link which would take you to the subtitling page.</p>
+<b>Step 2:</b>
+<p><img src="../../images/captioning2.png" /><br/>
+    Choose the language the video is in and the language you want to subtitle into and press continue</p>
+<b>Step 3:</b>
+<p><img src="../../images/captioning3.png" /><br/>
+    You can always watch the video which explains how AMARA should be used.</p>
+<p>Click continue to move to the next screen. </p>
+<b>Step 4:</b>
+<p><img src="../../images/captioning4.png" /><br/>
+    This is the first phase, Typing. As you watch the video type everything that people say and all the important text that appears on-screen.</p>
+<p>Use Tab key to play next 4 seconds and after finishing one caption, press Enter to start a new caption.</p>
+<b>Step 5:</b>
+<p><img src="../../images/captioning5.png" /><br/>
+    The next phase is the syncing phase where you sync all the captions you have created. Press the "down" arrow right when each subtitle should appear.</p>
+<p>You can always add new captions in this step</p>
+<b>Step 6:</b>
+<p><img src="../../images/captioning6.png" /><br/>
+    Choose a title and description for these subtitles.</p>
+<b>Step 7:</b>
+<p><img src="../../images/captioning7.png" /><br/>
+    Check your work in this phase and you can even refine your captions in this phase by following the tips for making high quality subtitles.</p>
+<b>Step 8:</b>
+<p><img src="../../images/captioning8.png" /><br/>
+    Click on the "Download Subtitles" button and choose the format of the subtitles.</p>
+<p>Paste it in a text editor and save it in the desired format.</p>
+<b>Step 9:</b>
+<p>Now you can use the saved captions file and insert it in the media tag</p>
 <p>Please check out <a href="http://www.amara.org/en/">Amara website</a> for further instructions.</p>
 
 <?php require('../common/body_footer.inc.php'); ?>
